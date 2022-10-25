610258a021918cc2047452fef6c61a8c0d57129c
+ <+>
2022-10-25 01:07:03 -0400

    controller

--- app/controllers/animes_controller.rb
+++ app/controllers/animes_controller.rb
@@ -20,30 +20,23 @@ class AnimesController < ApplicationController
         render json: anime, status: :created
     end
 
-    # def update
-    #     anime = Anime.find(params[:id])
-    #     anime.update(anime_params)
-    #     render json: anime, status: :ok
-    # end
     def destroy
         Anime.find(params[:id]).destroy
         head :no_content
     end
 
-    # def avgrating
-    #     sum = 0
-    #     reviews =  Anime.find(params[:id]).reviews
-    #     reviews.each{|review| sum += review.rating}
-    #     sum.fdiv(reviews.size)
-    # end
+    def avgrating
+        sum = 0
+        reviews = Anime.find(params[:id]).reviews
+        reviews.each{|review| sum += review.rating}
+        sum.fdiv(reviews.size)
+    end
+
+
     private 
     def anime_params
        params.permit([:id, :title, :image, :year, :genre, :summary]) 
     end
-
-    # def authorize
-    #     render json: {error: "Please sign in to add a recipe" }, status: :unauthorized unless session.include? :user_id
-    # end
     
     def invalid
         render json: { error: "Invalid" }, status: :unprocessable_entity
